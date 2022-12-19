30a1b277a34a5faa5b59e85ac91e127318d74a2a
Sonwanseo <sonwanseo@gmail.com>
2022-12-19 23:40:16 +0900

    feat: Svg 컴포넌트 추가

---
+++ src/components/Common/Svg/Svg.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Svg, { SvgProps } from 'react-native-svg';
+
+type Props = SvgProps & {
+  size?: number;
+};
+
+export default function (props: Props): React.ReactElement {
+  const { width, height, size = 1, ...rest } = props;
+
+  return (
+    <Svg
+      focusable={false}
+      height={height ? Number(height) * size : undefined}
+      pointerEvents="none"
+      viewBox={`0 0 ${width || 24} ${height || 24}`}
+      width={width ? Number(width) * size : undefined}
+      {...rest}
+    />
+  );
+}
