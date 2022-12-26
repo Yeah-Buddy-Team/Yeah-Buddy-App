918e7e6c083cdc845d470d8d2dc55a9a8386f90f
Sonwanseo <sonwanseo@gmail.com>
2022-12-26 23:48:59 +0900

    feat: 운동 계획 Flow 화면 초기 설정

---
+++ src/screens/SelectWorkoutExerciseByBodyPart/SelectWorkoutExerciseByBodyPart.tsx
@@ -0,0 +1,5 @@
+import { View } from '../../components/Common';
+
+export function SelectWorkoutExerciseByBodyPart() {
+  return <View />;
+}
--- src/types/System/Navigation.ts
+++ src/types/System/Navigation.ts
@@ -5,6 +5,9 @@ export enum RootStack {
   SignIn = 'SignIn', // 로그인 화면
   Root = 'Root', // 바텀 탭 네비게이션 화면
   WorkoutPlan = 'WorkoutPlan', // 운동 계획 화면
+  SelectWorkoutStimulationBodyPart = 'SelectWorkoutStimulationBodyPart', // 운동 계획 자극 부위 선택 화면
+  SelectWorkoutExerciseByBodyPart = 'SelectWorkoutExerciseByBodyPart', // 자극 부위 별 운동 선택 화면
+  MakeWorkoutPlan = 'MakeWorkoutPlan', // 운동 계획 세우기 화면
   Calendar = 'Calendar', // 캘린더 화면
   Friends = 'Friends', // 친구 화면
   MyProfile = 'MyProfile', // 마이 화면
