2e63ced5d76d9cc8b64b0672476f5f1e08cf9baf
Sonwanseo <sonwanseo@gmail.com>
2023-01-03 00:05:07 +0900

    feat: 자극 부위 별 운동 선택 화면 뷰 & 뷰로직 추가

--- src/screens/SelectWorkoutExerciseByBodyPart/SelectWorkoutExerciseByBodyPart.tsx
+++ src/screens/SelectWorkoutExerciseByBodyPart/SelectWorkoutExerciseByBodyPart.tsx
@@ -1,13 +1,94 @@
-import { Button } from 'react-native';
+import { ScrollView } from 'react-native';
+import { View } from '../../components/Common';
 import { HeaderLayout } from '../../layouts';
 import { useSelectWorkoutExerciseByBodyPart } from './useSelectWorkoutExerciseByBodyPart';
+import { BodyPart, Exercise } from './components';
+import { COLORS } from '../../constants';
+import TempExerciseImg from '../../assets/images/TempExerciseImg.png';
+
+export const BodyParts = [
+  '팔(이두)',
+  '팔(삼두)',
+  '어깨',
+  '가슴',
+  '등',
+  '하체',
+  '유산소',
+  '기타',
+];
+
+const tempArr = [
+  '가',
+  '나',
+  '다',
+  '라',
+  '마',
+  '바',
+  '사',
+  '아',
+  '자',
+  '차',
+  '카',
+  '타',
+  '파',
+  '하',
+];
 
 export function SelectWorkoutExerciseByBodyPart() {
-  const { addExerciseToWorkoutPlan } = useSelectWorkoutExerciseByBodyPart();
+  const { selectedBodyPart, addExerciseToWorkoutPlan, changeBodyPart } =
+    useSelectWorkoutExerciseByBodyPart();
 
   return (
     <HeaderLayout headerTitle="운동 시작하기">
-      <Button title="ㅁㄴㅇㄹ" onPress={addExerciseToWorkoutPlan} />
+      <View fill>
+        <ScrollView
+          overScrollMode="never"
+          horizontal
+          style={{
+            flexGrow: 0,
+          }}
+          contentContainerStyle={{
+            paddingHorizontal: 20,
+            paddingVertical: 12,
+          }}
+          showsHorizontalScrollIndicator={false}
+        >
+          {BodyParts.map((item, index) => (
+            <BodyPart
+              onPress={changeBodyPart}
+              label={item}
+              key={item}
+              selected={selectedBodyPart === item}
+              last={index === 7}
+            />
+          ))}
+        </ScrollView>
+        <ScrollView
+          style={{
+            flex: 1,
+          }}
+          overScrollMode="never"
+          showsVerticalScrollIndicator={false}
+        >
+          {tempArr.map(item => (
+            <>
+              <Exercise
+                selected={true}
+                name={item}
+                thumbnail={TempExerciseImg}
+              />
+              <View
+                style={{
+                  marginHorizontal: 22,
+                  height: 0,
+                  borderColor: COLORS.GRAY[200],
+                  borderWidth: 0.5,
+                }}
+              />
+            </>
+          ))}
+        </ScrollView>
+      </View>
     </HeaderLayout>
   );
 }
--- src/screens/SelectWorkoutExerciseByBodyPart/useSelectWorkoutExerciseByBodyPart.tsx
+++ src/screens/SelectWorkoutExerciseByBodyPart/useSelectWorkoutExerciseByBodyPart.tsx
@@ -1,4 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
+import React from 'react';
 import { RootStack, RootStackNavigationProps } from '../../types/System';
 
 export const useSelectWorkoutExerciseByBodyPart = () => {
@@ -6,12 +7,20 @@ export const useSelectWorkoutExerciseByBodyPart = () => {
     useNavigation<
       RootStackNavigationProps[RootStack.SelectWorkoutExerciseByBodyPart]
     >();
+  const [selectedBodyPart, setSelectedBodyPart] =
+    React.useState<string>('팔(이두)');
 
   const addExerciseToWorkoutPlan = () => {
     navigation.push(RootStack.MakeWorkoutPlan);
   };
 
+  const changeBodyPart = (targetBodyPart: string) => {
+    setSelectedBodyPart(targetBodyPart);
+  };
+
   return {
+    selectedBodyPart,
     addExerciseToWorkoutPlan,
+    changeBodyPart,
   };
 };
