d8163f755828b8118f68411e5a45af6912f74565
Sonwanseo <sonwanseo@gmail.com>
2023-01-11 23:37:32 +0900

    feat: Exercise Model 타입 정의

---
+++ src/types/Model/Exercise.ts
@@ -0,0 +1,12 @@
+export type StimulationBodyPart = {
+  key: string;
+  name: string;
+  order: number;
+};
+
+export type Exercise = {
+  id: number;
+  name: string;
+  order: number;
+  thumnbnailUrl: string;
+};
