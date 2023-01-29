a52c8cc09aad99ef03be9959b3f89d46d80a51a0
Sonwanseo <sonwanseo@gmail.com>
2023-01-29 20:42:58 +0900

    publish: 운동 자극 부위 선택 화면 개발

--- src/screens/SelectWorkoutStimulationBodyPart/SelectWorkoutStimulationBodyPart.tsx
+++ src/screens/SelectWorkoutStimulationBodyPart/SelectWorkoutStimulationBodyPart.tsx
@@ -1,13 +1,39 @@
-import { Button } from 'react-native';
+import { View } from '../../components/Common';
+import { WorkoutStimulationBodyPartObj } from '../../constants';
 import { HeaderLayout } from '../../layouts';
+import { WorkoutStimulationBodyPart } from './components';
 import { useSelectWorkoutStimulationBodyPart } from './useSelectWorkoutStimulationBodyPart';
 
 export function SelectWorkoutStimulationBodyPart() {
-  const { confirmStimulationBodyPart } = useSelectWorkoutStimulationBodyPart();
+  const { workoutStimulationBodyParts, confirmStimulationBodyPart } =
+    useSelectWorkoutStimulationBodyPart();
 
   return (
     <HeaderLayout headerTitle="운동 시작하기">
-      <Button title="ㅁㄴㅇㄹ" onPress={confirmStimulationBodyPart} />
+      <View
+        style={{
+          paddingTop: 20,
+          paddingHorizontal: 20,
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+        }}
+        row
+      >
+        {workoutStimulationBodyParts.map(item => {
+          if (!WorkoutStimulationBodyPartObj.hasOwnProperty(item.key)) return;
+          const target = WorkoutStimulationBodyPartObj[item.key];
+
+          return (
+            <WorkoutStimulationBodyPart
+              color={target.color}
+              key={item.key}
+              label={target.name}
+              icon={target.icon}
+              onPress={confirmStimulationBodyPart}
+            />
+          );
+        })}
+      </View>
     </HeaderLayout>
   );
 }
--- src/screens/SelectWorkoutStimulationBodyPart/useSelectWorkoutStimulationBodyPart.tsx
+++ src/screens/SelectWorkoutStimulationBodyPart/useSelectWorkoutStimulationBodyPart.tsx
@@ -1,4 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { WorkoutService } from '../../services';
+import { StimulationBodyPart } from '../../types/Model';
 import { RootStack, RootStackNavigationProps } from '../../types/System';
 
 export const useSelectWorkoutStimulationBodyPart = () => {
@@ -7,11 +10,26 @@ export const useSelectWorkoutStimulationBodyPart = () => {
       RootStackNavigationProps[RootStack.SelectWorkoutStimulationBodyPart]
     >();
 
+  const [workoutStimulationBodyParts, setWorkoutStimulationBodyParts] =
+    React.useState<StimulationBodyPart[]>([]);
+
   const confirmStimulationBodyPart = () => {
     navigation.push(RootStack.SelectWorkoutExerciseByBodyPart);
   };
 
+  const getWorkoutStimulationBodyPart = async () => {
+    const result = await WorkoutService.getWorkoutStimulationBodyPartByOrder();
+    if (!result) return;
+
+    setWorkoutStimulationBodyParts(result);
+  };
+
+  React.useEffect(() => {
+    (async () => await getWorkoutStimulationBodyPart())();
+  }, []);
+
   return {
+    workoutStimulationBodyParts,
     confirmStimulationBodyPart,
   };
 };
