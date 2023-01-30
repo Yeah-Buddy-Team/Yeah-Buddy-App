bdab411f9aa498c006c41599d4e048e7de542931
Sonwanseo <sonwanseo@gmail.com>
2023-01-30 13:25:52 +0900

    feat: WorkoutAPI에 자극 부위 별 운동 조회 API 연결하기

--- src/api/CONSTANTS.ts
+++ src/api/CONSTANTS.ts
@@ -1,5 +1,8 @@
 export const POST_KAKAO_SIGN_IN = (accessToken: string, refreshToken: string) =>
   `/login/oauth/KAKAO?accessToken=${accessToken}&refreshToken=${refreshToken}`;
 
+// Workout
 export const GET_WORKOUT_STIMULATION_BODY_PARTS = () =>
   `/workout-stimulation-body-parts`;
+export const GET_WORKOUT_EXERCISE_BY_BODY_PART = (bodyPart: string) =>
+  `/workouts?bodyPart=${bodyPart}`;
--- src/api/WorkoutAPI.ts
+++ src/api/WorkoutAPI.ts
@@ -1,4 +1,7 @@
-import { GET_WORKOUT_STIMULATION_BODY_PARTS } from './CONSTANTS';
+import {
+  GET_WORKOUT_EXERCISE_BY_BODY_PART,
+  GET_WORKOUT_STIMULATION_BODY_PARTS,
+} from './CONSTANTS';
 import axios from './default';
 
 export const getWorkoutStimulationBodyParts = async () => {
@@ -50,3 +53,119 @@ export const getWorkoutStimulationBodyParts = async () => {
     console.error(e.message);
   }
 };
+
+export const getWorkoutExerciseByBodyPart = async (bodyPart: string) => {
+  try {
+    // const url = GET_WORKOUT_EXERCISE_BY_BODY_PART(bodyPart);
+
+    // const response = await axios.get(url);
+
+    // return response.data;
+    return [
+      {
+        id: 1,
+        name: '가',
+        category: '',
+        order: 1,
+      },
+
+      {
+        id: 2,
+        name: '나',
+        category: '',
+        order: 2,
+      },
+      {
+        id: 3,
+        name: '다',
+        category: '',
+        order: 3,
+      },
+      {
+        id: 4,
+        name: '라',
+        category: '',
+        order: 4,
+      },
+      {
+        id: 5,
+        name: '마',
+        category: '',
+        order: 5,
+      },
+      {
+        id: 6,
+        name: '바',
+        category: '',
+        order: 6,
+      },
+      {
+        id: 7,
+        name: '사',
+        category: '',
+        order: 7,
+      },
+      {
+        id: 8,
+        name: '아',
+        category: '',
+        order: 8,
+      },
+      {
+        id: 9,
+        name: '자',
+        category: '',
+        order: 9,
+      },
+      {
+        id: 10,
+        name: '차',
+        category: '',
+        order: 10,
+      },
+      {
+        id: 11,
+        name: '카',
+        category: '',
+        order: 11,
+      },
+      {
+        id: 12,
+        name: '타',
+        category: '',
+        order: 12,
+      },
+      {
+        id: 13,
+        name: '파',
+        category: '',
+        order: 13,
+      },
+      {
+        id: 14,
+        name: '하',
+        category: '',
+        order: 14,
+      },
+    ];
+  } catch (e: any) {
+    console.error(e.message);
+  }
+};
+
+const tempArr = [
+  '가',
+  '나',
+  '다',
+  '라',
+  '마',
+  '바',
+  '사',
+  '아',
+  '자',
+  '차',
+  '카',
+  '타',
+  '파',
+  '하',
+];
