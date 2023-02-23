d88b5bdb955e171118c964c7f2999e5fc66b7f46
Sonwanseo <sonwanseo@gmail.com>
2023-02-23 23:27:16 +0900

    feat: 스토리지 저장 관련 유틸 함수 추가

---
+++ src/utils/storage.ts
@@ -0,0 +1,88 @@
+import AsyncStorage from '@react-native-community/async-storage';
+import EncryptedStorage from 'react-native-encrypted-storage/lib/typescript/EncryptedStorage';
+
+export const setItemAsyncStorage = async (
+  key: string,
+  value: unknown,
+  callback?: () => void,
+) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value), callback);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const getItemAsyncStorage = async (
+  key: string,
+  callback?: () => void,
+) => {
+  try {
+    const value = (await AsyncStorage.getItem(key, callback)) ?? 'null';
+
+    return JSON.parse(value);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const removeItemAsyncStorage = async (
+  key: string,
+  callback?: () => void,
+) => {
+  try {
+    await AsyncStorage.removeItem(key, callback);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const clearAsyncStorage = async (callback?: () => void) => {
+  try {
+    await AsyncStorage.clear(callback);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const setItemEncryptedStorage = async (key: string, value: unknown) => {
+  try {
+    await EncryptedStorage.setItem(key, JSON.stringify(value));
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const getItemEncryptedStorage = async (key: string) => {
+  try {
+    const value = (await EncryptedStorage.getItem(key)) ?? 'null';
+
+    return value;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const removeItemEncryptedStorage = async (key: string) => {
+  try {
+    await EncryptedStorage.removeItem(key);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+export const clearEncryptedStorage = async () => {
+  try {
+    await EncryptedStorage.clear();
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
