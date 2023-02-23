d91f38009ff889a5275b8f3b2919912cbc7bb608
Sonwanseo <sonwanseo@gmail.com>
2023-02-23 23:59:38 +0900

    feat: 로그인 성공 후 토큰 저장

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,6 +1,7 @@
 import KakaoSDK from '@actbase/react-kakaosdk';
 import Config from 'react-native-config';
 import { AuthAPI } from '../api';
+import { setItemEncryptedStorage } from '../utils';
 
 export const kakaoSignIn = async () => {
   try {
@@ -12,6 +13,9 @@ export const kakaoSignIn = async () => {
     AuthAPI.postKaKaoSignIn({
       accessToken: tokens.access_token,
       refreshToken: tokens.refresh_token,
+    }).then(data => {
+      setItemEncryptedStorage('ACCESS_TOKEN', data.accessToken);
+      setItemEncryptedStorage('REFRESH_TOKEN', data.refreshToken);
     });
   } catch (e: any) {
     console.error(e.message);
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import EncryptedStorage from 'react-native-encrypted-storage/lib/typescript/EncryptedStorage';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 export const setItemAsyncStorage = async (
   key: string,
