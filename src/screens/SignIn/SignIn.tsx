1aa714b90d64aa1591bcfce4c6e4ec6d228ee1a9
Sonwanseo <sonwanseo@gmail.com>
2023-04-08 00:09:16 +0900

    publish: 로그인 화면 디자인 변경

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Image, TouchableOpacity, useWindowDimensions } from 'react-native';
 import KakaoSignInBtn from '../../assets/images/KakaoSignInBtn.png';
-import Logo from '../../assets/images/Logo.png';
-import { View } from '../../components/Common';
+import TextLogo from '../../assets/images/TextLogo.png';
+import { Text, View } from '../../components/Common';
 import { COLORS } from '../../constants';
 import { useSignIn } from './useSignIn';
+import { INDIGO, WHITE } from '../../constants/Theme';
 
 export function SignIn() {
   const { width: WIDTH } = useWindowDimensions();
@@ -21,32 +22,43 @@ export function SignIn() {
       }}
     >
       <Image
-        source={Logo}
+        source={TextLogo}
         style={{
           resizeMode: 'contain',
-          width: 72,
-          height: 111,
+          width: '71.5%',
           top: '30%',
         }}
       />
-      <TouchableOpacity
+      <View
         style={{
-          width: WIDTH - 40,
-          height: 48,
           marginBottom: '14.7%',
         }}
-        onPress={signInWithKakao}
-        activeOpacity={0.8}
       >
-        <Image
-          source={KakaoSignInBtn}
+        <View style={{ width: WIDTH - 40 }}>
+          <Text color={WHITE} subtitle semibold style={{ marginBottom: 14 }}>
+            환영합니다
+          </Text>
+          <Text color={INDIGO[100]} body3 style={{ marginBottom: 23 }}>
+            로그인하여 Yeah Buddy를 이용해 보세요!
+          </Text>
+        </View>
+        <TouchableOpacity
           style={{
-            resizeMode: 'contain',
-            width: '100%',
-            height: '100%',
+            height: 48,
           }}
-        />
-      </TouchableOpacity>
+          onPress={signInWithKakao}
+          activeOpacity={0.8}
+        >
+          <Image
+            source={KakaoSignInBtn}
+            style={{
+              resizeMode: 'cover',
+              width: '100%',
+              height: '100%',
+            }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
