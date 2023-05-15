14529422d89b96463861b5fd6188341953c7b8c0
Sonwanseo <sonwanseo@gmail.com>
2023-05-15 21:45:03 +0900

    feat: 운동 계획 홈 화면 퍼블리시

--- src/screens/WorkoutPlan/WorkoutPlan.tsx
+++ src/screens/WorkoutPlan/WorkoutPlan.tsx
@@ -1,13 +1,41 @@
-import { Button } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 import { View } from '../../components/Common';
 import { useWorkoutPlan } from './useWorkoutPlan';
+import TextLogoBlack from '../../assets/images/TextLogoBlack.png';
+import WorkoutPlanButton from '../../assets/images/WorkoutPlanButton.png';
 
 export function WorkoutPlan() {
   const { createWorkoutPlan } = useWorkoutPlan();
 
   return (
-    <View>
-      <Button onPress={createWorkoutPlan} title="ㅁㄴㅇㄹ" />
+    <View
+      style={{
+        alignItems: 'center',
+      }}
+    >
+      <Image
+        style={{
+          width: '80%',
+          top: -40,
+          resizeMode: 'center',
+          marginBottom: -40,
+        }}
+        source={TextLogoBlack}
+      />
+      <TouchableOpacity
+        style={{ flex: 1, width: '90%', top: -50 }}
+        activeOpacity={0.7}
+        onPress={createWorkoutPlan}
+      >
+        <Image
+          style={{
+            width: '100%',
+            height: 140,
+            resizeMode: 'center',
+          }}
+          source={WorkoutPlanButton}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
